b0241aa65b3c316d4fc96d891c90893292abff67
FatalMa3lstrom <aaroneverson03@gmail.com>
2024-08-09 18:14:42 +1000

    Attenuation
    I FIGURED OUT THE SPACIAL AUDIO AT LEAST!

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,17 +14,26 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_DRY_WIND_SFX = 471389562U;
+        static const AkUniqueID PLAYBIRDCALL = 2103452570U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BIRDCALL = 357926156U;
         static const AkUniqueID DRY_WIND = 909382117U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NATURE = 3501062356U;
+        static const AkUniqueID NPC = 662417162U;
+        static const AkUniqueID NPCAMBIENT = 623496326U;
+        static const AkUniqueID NPCDIALOGUE = 1141413220U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
