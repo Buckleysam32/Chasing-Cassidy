b007c4e71e2dbd6f2c8f47bfe193c45f73519f97
FatalMa3lstrom <aaroneverson03@gmail.com>
2024-09-28 10:04:31 +1000

    Footsteps Setup
    Edited Player Movement Script to allow wise to detect layers, set up a float timer, and play the appropriate footstep audio. Audio has not been implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AK.Wwise;
+using System.Collections;
+
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -27,7 +29,17 @@ public class PlayerMovement : MonoBehaviour
     {
         GetInput();
         MovePlayer();
-        //PlayFootstepSound();
+        CheckTerrain();
+
+        if (isWalking)
+        {
+            if (timer > footstepSpeed)
+            {
+                SelectAndPlayFootstep();
+                timer = 0.0f;
+            }
+            timer += Time.deltaTime;
+        }
 
         //camAnim.SetBool("isWalking", isWalking);
 
@@ -62,12 +74,85 @@ public class PlayerMovement : MonoBehaviour
         myCC.Move(movementVector * Time.deltaTime);
     }
 
-    void PlayFootstepSound()
+    //Footstep Audio
+
+    private enum CURRENT_TERRAIN { SAND, WOOD, COMPACTSAND };
+
+    [SerializeField]
+    private CURRENT_TERRAIN currentTerrain;
+
+    [SerializeField]
+    private AK.Wwise.Event footstepsEvent;
+
+    [SerializeField]
+    private AK.Wwise.Switch[] terrainSwitch;
+
+    //Detecting Layer
+
+    private void CheckTerrain()
     {
-        /*
-        if (isWalking && !footstepAudioSource.isPlaying)
+        RaycastHit[] hit;
+
+        hit = Physics.RaycastAll(transform.position, Vector3.down, 10.0f);
+
+        foreach (RaycastHit rayhit in hit)
         {
-            footstepAudioSource.PlayOneShot(footstepSound);
-        }*/
+            if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Sand"))
+            {
+                currentTerrain = CURRENT_TERRAIN.SAND;
+            }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Wood"))
+            {
+                currentTerrain = CURRENT_TERRAIN.WOOD;
+            }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("CompactSand"))
+            {
+                currentTerrain = CURRENT_TERRAIN.COMPACTSAND;
+            }
+        }
+    }
+
+    //Playing Correct Audio
+
+    private void PlayFootstep(int terrain)
+    {
+        terrainSwitch[terrain].SetValue(this.gameObject);
+        AkSoundEngine.PostEvent(footstepsEvent.Id, this.gameObject);
+    }
+
+    public void SelectAndPlayFootstep()
+    {
+        switch (currentTerrain)
+        {
+            case CURRENT_TERRAIN.SAND:
+                PlayFootstep(0);
+                break;
+
+            case CURRENT_TERRAIN.WOOD:
+                PlayFootstep(1);
+                break;
+
+            case CURRENT_TERRAIN.COMPACTSAND:
+                PlayFootstep(2);
+                break;
+
+            default:
+                PlayFootstep(0);
+                break;
+        }
+    }
+
+    //Establishing Float Timer
+
+    private Rigidbody rb;
+
+    float timer = 0.0f;
+
+    [SerializeField]
+    float footstepSpeed = 0.3f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 }
--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,27 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPSTERRAIN
+        {
+            static const AkUniqueID GROUP = 665703777U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID COMPACTSAND = 2488754382U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPSTERRAIN
+
+    } // namespace SWITCHES
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
